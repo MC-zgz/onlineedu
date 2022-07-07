ea423b9556dc0c0d3b38c9695768067e673e1846
MC-zgz <76649764+MC-zgz@users.noreply.github.com>
2022-07-07 21:50:36 +0800

    Update AliyunVodSDKUtils.java

--- online-edu-java/service/service-vod/src/main/java/com/zgz/servicevod/utils/AliyunVodSDKUtils.java
+++ online-edu-java/service/service-vod/src/main/java/com/zgz/servicevod/utils/AliyunVodSDKUtils.java
@@ -10,7 +10,7 @@ import com.aliyuncs.profile.DefaultProfile;
  */
 public class AliyunVodSDKUtils {
     public static DefaultAcsClient initVodClient(String accessKeyId, String accessKeySecret) throws ClientException {
-        String regionId = "cn-shenzhen";  // 点播服务接入区域
+        String regionId = "131qdadsasdasd";  // 点播服务接入区域
         DefaultProfile profile = DefaultProfile.getProfile(regionId, accessKeyId, accessKeySecret);
         DefaultAcsClient client = new DefaultAcsClient(profile);
         return client;
